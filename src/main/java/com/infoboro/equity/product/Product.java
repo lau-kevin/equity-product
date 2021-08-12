1f1b0daf6518ce6fe46432de9d5938250b67653d
Kevin Lau <whkevinlau@gmail.com>
2021-08-12 17:48:20 +0800

    1st commit

---
+++ src/main/java/com/infoboro/equity/product/Product.java
@@ -0,0 +1,19 @@
+package com.infoboro.equity.product;
+
+
+import org.apache.logging.log4j.*;
+
+public class Product {
+	
+	private static final Logger logger = LogManager.getLogger(Product.class);
+	
+	public static void main(String[] args) {
+		logger.debug("this is class Product.");
+		
+	}
+	
+	
+	
+	
+	
+}
